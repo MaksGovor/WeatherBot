2a00ce0e30edfb12afbd431f2b83188fd3184e4d
MaksGovor <maksgovruha@gmail.com>
2020-04-30 19:30:37 +0300

    Add city.list && do test api request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-const { Telegraf } = require('telegraf');
-const consfig = require('./config.json');
+'use strict';
 
-const bot = new Telegraf (consfig.token, consfig.options);
+const { Telegraf } = require('telegraf');
+const Extra = require('telegraf/extra');
+const Markup = require('telegraf/markup');
+const request = require('request');
+const config = require('./config.json');
+const TOKEN = process.env.token || config.token;
+const apiKey = config["api-key"];
+
+const bot = new Telegraf(TOKEN, config.options);
+
+const keyboard = Markup.inlineKeyboard([
+  Markup.urlButton('author', 'https://github.com/MaksGovor'),
+  Markup.callbackButton('Delete', 'delete')
+]);
 
 bot.start(ctx => ctx.reply('Hello'));
+
 //bot.launch();
 
-console.log(consfig.options);
+//bot.launch();
+
+request('https://api.openweathermap.org/data/2.5/weather?q=Kieve&appid=' + apiKey,
+  (err, reaponse, data) => {
+    data = JSON.parse(data);
+    if (!err) console.dir(data);
+});
+
+console.log(config.options);
