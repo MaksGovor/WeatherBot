c3ea73b9601e3ede38038356dbc5a36823e5a127
MaksGovor <maksgovruha@gmail.com>
2020-05-01 14:35:06 +0300

    Add parse data by request

--- src/index.js
+++ src/index.js
@@ -16,13 +16,14 @@ const projection = metadata => {
   const mapper = data => {
     const hash = new Object();
     for (const key of keys) {
-      const def = metadata[key];
-      const [name, transform] = def;
+      const definition = metadata[key];
+      const [name, transform] = definition;
       let val = data[name];
       if (val) {
         if (transform) {
           val = typeof transform === 'function' ?
-            transform(val) : val.map(projection(transform));
+            transform(val) : Array.isArray(val) ? 
+              val.map(projection(transform)) : val = projection(transform)(val);
         }
         hash[key] = val;
       }
@@ -36,16 +37,37 @@ const projection = metadata => {
 // Usage 
 
 const mdFor5Day = {
+  list: ['list', {
+    temp: ['main', {
+      average: ['temp', t => `${Math.floor(+t - 273)}℃`],
+      feelsLike: ['feels_like', t => `${Math.floor(+t - 273)}℃`],
+      min: ['temp_min', t => `${Math.floor(+t - 273)}℃`],
+      max: ['temp_max', t => `${Math.floor(+t - 273)}℃`],    
+    }],
+    weather: ['weather', {
+      main: ['main', x => `At that time the weather is: ${x}`],
+      description: ['description', x => `More about the weather: ${x}`],
+    }],
+    wind: ['wind', {
+      speed: ['speed', s => `The wind speed is ${s} m/s`],
+    }],
+    rain: ['rain', r => `Chanse of presipitation is ${r['3h']}`],
+    date: ['dt_txt', d => d],
+  }],
+  city: ['city', {
+    name: ['name', x => x],
+    country: ['country', x => x],
+    sunrise: ['sunrise', x => new Date(x*1000).toLocaleString()],
+    sunset: ['sunset', x => new Date(x*1000).toLocaleString()],
+  }]
 }
+
+const testp = projection(mdFor5Day);
 
 const keyboard = Markup.inlineKeyboard([
   Markup.urlButton('author', 'https://github.com/MaksGovor'),
   Markup.callbackButton('Delete', 'delete')
 ]);
-
-//bot.on('message', ctx => {
-//  console.log(ctx.message);
-//});
 
 bot.start(ctx => ctx.reply('Hello'));
 
@@ -63,13 +85,28 @@ bot.command('weather', ctx => {
   });
 })
 
-//bot.launch();
+bot.command('weather5days', ctx => {
+  const text = getTownFromMsg(ctx.message.text);
+  console.log(text);
+  request('https://api.openweathermap.org/data/2.5/forecast?q=' + text + '&appid=' + apiKey,
+    (err, reaponse, data) => {
+      try {
+        data = JSON.parse(data);
+        if (!err) ctx.reply(data.list.map(testp)[0]);
+      } catch (err) {
+        ctx.reply('!!!Error ' + err.message);
+      }
+  });
+})
+
+
+bot.launch();
 
 request('https://api.openweathermap.org/data/2.5/forecast?q=Krolevets&appid=' + apiKey,
   (err, reaponse, data) => {
     if (!err){
       data = JSON.parse(data);
-      console.dir(data, {depth: 3});
+      console.dir(testp(data), {depth: 2});
     }
   }
 )
