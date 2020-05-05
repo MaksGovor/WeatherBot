bb892e8eaa2e1e16cb79f48fe01a876ab650e1cd
MaksGovor <maksgovruha@gmail.com>
2020-05-05 11:10:19 +0300

    Update helper

--- src/display.js
+++ src/display.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const helper = src => {
-  if (Array.isArray(src)) src = src.shift();
-  const keys = Object.keys(src);
-  const res = [src].map(obj => keys.map(
-    (key) => obj[key] instanceof Object ? helper(obj[key]) : obj[key].toString()
+  const iterable = Array.isArray(src) ? src : [src];
+  const keys = Object.keys(iterable[0]);
+  const res = iterable.map(obj => keys.map(
+    key => obj[key] instanceof Object ? helper(obj[key]) : obj[key].toString()
   ));
   return res.map(row => row.join('\n')).join('\n');
 };
