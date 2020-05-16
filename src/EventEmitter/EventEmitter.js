ecb00a8c3452b737374d904f51e0afc4623a3940
MaksGovor <maksgovruha@gmail.com>
2020-05-16 12:00:10 +0300

    Add remove and once method

--- src/EventEmitter/EventEmitter.js
+++ src/EventEmitter/EventEmitter.js
@@ -6,7 +6,7 @@ function EventEmitter () {
 }
 
 EventEmitter.prototype.on = function(name, fn) {
-  const event = this.get(name);
+  const event = this.events.get(name);
   if (event) event.add(fn);
   else this.events.set(name, new Set([fn]));
 };
@@ -15,7 +15,32 @@ EventEmitter.prototype.emit = function(name, ...data) {
   const event = this.events.get(name);
   if (event) {
     for (const fn of event.values()) {
-        fn(...data);
+      fn(...data);
     }
   }
 };
+
+EventEmitter.prototype.remove = function(name, fn) {
+  const event = this.events.get(name);
+  if (!event) return;
+  if (event.has(fn)) {
+    event.delete(fn);
+    return;
+  }
+  const wrapper = this.wrappers.get(fn);
+  if (wrapper) {
+    event.delete(wrapper);
+    if (event.size === 0) this.events.delete(name);
+  }
+}
+
+EventEmitter.prototype.once = function(name, fn) {
+  const wrapper = (...args) => {
+    this.remove(name, wrapper);
+    fn(...args);
+  };
+  this.wrappers.set(fn, wrapper);
+  this.on(name, wrapper);
+}
+
+module.exports = EventEmitter;
